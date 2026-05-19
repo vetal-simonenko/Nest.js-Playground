0bdaa4069d4d2f9e897ce4d968385f658288ef46
VitaliiSimonenko <Vitalii_Simonenko@epam.com>
2026-05-19 15:23:01 +0300

    Commit message

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    credentials: true,
+    origin: [process.env.CLIENT_URL],
+    exposeHeaders: 'set-cookie',
+  });
+
+  await app.listen(5000);
 }
 
 void bootstrap();
